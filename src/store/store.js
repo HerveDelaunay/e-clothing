3bcf491e0a0ebe14c8602cbb7e6b5e1443ccd8ab
HerveDelaunay <delaunay.herve06@gmail.com>
2023-02-15 14:36:34 +0100

    redux persist added for the storage of the app state

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,29 @@
-import { compose, applyMiddleware } from 'redux';
-import { legacy_createStore as createStore } from 'redux'
-import logger from 'redux-logger';
+import { compose, applyMiddleware } from "redux";
+import { legacy_createStore as createStore } from "redux";
+import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import { rootReducer } from './root-reducer';
+import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
